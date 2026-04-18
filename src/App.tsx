f39cba46f5294c7c1a2703c5ae6d5df0fbcfc037
JuliusCRealtair <julius.cadungog@rea-group.com>
2026-04-19 06:20:11 +0800

    update App component: add input field for position and manage state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { Container, Box, Typography, TextField } from "@mui/material";
 import "./App.css";
 import MainGrid from "./components/MainGrid";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("1,1 NORTH");
+
   return (
-    <>
-      <MainGrid input="1,1 NORTH" />
-    </>
+  <Container maxWidth="md">
+        <Box sx={{ my: 4 }}>
+          <TextField
+            fullWidth
+            label="Position Input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="1,1 NORTH"
+            margin="normal"
+          />
+          <Box sx={{ mt: 3 }}>
+            <MainGrid input={input} />
+          </Box>
+        </Box>
+      </Container>
   );
 }
 
